a0ccc3ea6ddcce276d6744b95818e6d317d31aac
Ara Ovsepyan <32188396+ARAOvsepyan@users.noreply.github.com>
2024-02-28 00:20:57 +0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,9 +1,17 @@
+require('dotenv').config()
+
+const express = require('express')
 const cors = require('cors')
+const sequelize = require('./db')
 const cookieParser = require('cookie-parser')
 const router = require('./router/index');
 const errorMiddleware = require('./middleware/errorMiddleware')
 
 
+const PORT = process.env.PORT || 8080;
+const app = express();
+
+app.use(express.json());
 app.use(cookieParser());
 // app.use(cors({
 //     credentials: true,
@@ -11,3 +19,19 @@ app.use(cookieParser());
 // }));
 app.use('/api', router);
 app.use(errorMiddleware);
+
+
+const start = async () => {
+  try {
+    await sequelize.authenticate();
+    await sequelize.sync();
+
+    app.listen(PORT, () => {
+      console.log(`Server is running on Port ${PORT}`);
+    })
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+start();

62d2f1b21f833f05dc890fa0a6127002a8508675
Ara Ovsepyan <32188396+ARAOvsepyan@users.noreply.github.com>
2024-02-28 05:48:15 +0300

    Update apiError.js

--- server/error/apiError.js
+++ server/error/apiError.js
@@ -4,22 +4,22 @@ module.exports = class ApiError extends Error{
 
   constructor(status, message, errors) {
       super(message);
-      this.status = status
+      this.status = status;
   }
 
   static BadRequest(message, errors = []) {
-    return new ApiError(400, message, errors)
-  }
-
-  static internal(message) {
-    return new ApiError(500, message)
-  }
-
-  static forbidden(message) {
-    return new ApiError(403, message)
+    return new ApiError(400, message, errors);
   }
 
   static UnauthorizedError() {
-    return new ApiError(401, 'Пользователь не авторизован')
+    return new ApiError(401, 'Пользователь не авторизован');
+  }
+
+  static NotFound(message) {
+    return new ApiError(404, message);
+  }
+
+  static ServiceTemporarilyUnavailable(message) {
+    return new ApiError(503, message);
   }
 }

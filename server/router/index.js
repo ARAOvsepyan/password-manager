fa5a71fe6bd03e9da45b0516c94e117116f8cac9
Ara Ovsepyan <32188396+ARAOvsepyan@users.noreply.github.com>
2024-02-28 05:49:36 +0300

    Create employee route

--- server/router/index.js
+++ server/router/index.js
@@ -1,11 +1,11 @@
 const Router = require('express')
 const authRouter = require('./authRouter')
 const authMiddleware = require('../middleware/authMiddleware')
-const userRouter = require('./userRouter')
+const employeeRouter = require('./employeeRouter')
 
 const router = new Router()
 
 router.use('/auth', authRouter)
-// router.use('/user', authMiddleware, userRouter)
+router.use('/employee', authMiddleware, employeeRouter)
 
 module.exports = router

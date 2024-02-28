4e720a55651ff343a7bd796be7f904a930865cf5
Ara Ovsepyan <32188396+ARAOvsepyan@users.noreply.github.com>
2024-02-28 05:48:48 +0300

    Rename Users* to Admonistrator*

--- server/models/models.js
+++ server/models/models.js
@@ -1,35 +1,32 @@
 const { DataTypes } = require('sequelize')
 const sequelize = require('../db')
 
-const Users = sequelize.define('users', {
+const AdministratorModel = sequelize.define('administrator', {
   id: {type: DataTypes.UUID ,primaryKey: true, defaultValue: DataTypes.UUIDV1},
   email: {type: DataTypes.STRING, allowNull: false, unique: true},
   password: {type: DataTypes.STRING, allowNull: false},
 })
 
-const JWT_Token = sequelize.define('jwt_tokens', {
+const TokenModel = sequelize.define('jwt_token', {
   refreshToken: {type: DataTypes.STRING, allowNull: false}
 })
 
-const Users_Info = sequelize.define('users_info', {
+const EmployeeModel = sequelize.define('employee', {
   id: {type: DataTypes.INTEGER ,autoIncrement: true ,primaryKey: true},
-  last_name: {type: DataTypes.STRING(50), allowNull: false},
-  first_name: {type: DataTypes.STRING(50), allowNull: false},
-  patronymic: {type: DataTypes.STRING(50), allowNull: false},
-  position: {type: DataTypes.STRING(50), allowNull: false},
-  departament: {type: DataTypes.STRING(50), allowNull: false}
+  email: {type: DataTypes.STRING, allowNull: false, unique: true}, 
+  login: {type: DataTypes.STRING, allowNull: false, unique: true}, // Может стоит это вынести отделтно в общую таблицу с password
+  password: {type: DataTypes.STRING, allowNull: false}, // Может стоит это вынести отделтно в общую таблицу с login
+  last_name: {type: DataTypes.STRING, allowNull: false},
+  first_name: {type: DataTypes.STRING, allowNull: false},
+  patronymic: {type: DataTypes.STRING, allowNull: false},
+  position: {type: DataTypes.STRING, allowNull: false}, // Стоит вынести отдельно
+  departament: {type: DataTypes.STRING, allowNull: false} // Стоит вынести отдельно
 })
 
-Users.hasOne(JWT_Token, {
+AdministratorModel.hasOne(TokenModel, {
   foreignKey: {
     type: DataTypes.UUID
   }
 });
 
-Users.hasOne(Users_Info, {
-  foreignKey: {
-    type: DataTypes.UUID
-  }
-});
-
-module.exports = { Users, JWT_Token, Users_Info }
+module.exports = { AdministratorModel, TokenModel, EmployeeModel }

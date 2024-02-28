7dc296f305198ad5e97bfa317efc3c86ef396e38
Ara Ovsepyan <32188396+ARAOvsepyan@users.noreply.github.com>
2024-02-28 05:48:27 +0300

    Update authMiddleware.js

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -1,26 +1,32 @@
 const ApiError = require('../error/apiError');
-const { validateAccessToken } = require('../service/tokenService');
+const tokenService = require('../service/tokenService');
 
 module.exports = function (req, res, next) {
-    try {
-        const authorizationHeader = req.headers.authorization;
-        if (!authorizationHeader) {
-            return next(ApiError.UnauthorizedError());
-        }
+  try {
+    const authorizationHeader = req.headers.authorization;
 
-        const accessToken = authorizationHeader.split(' ')[1];
-        if (!accessToken) {
-            return next(ApiError.UnauthorizedError());
-        }
-
-        const userData = tokenService.validateAccessToken(accessToken);
-        if (!userData) {
-            return next(ApiError.UnauthorizedError());
-        }
-
-        req.user = userData;
-        next();
-    } catch (e) {
-        return next(ApiError.UnauthorizedError());
+    if (!authorizationHeader) {
+      console.log('1');
+      return next(ApiError.UnauthorizedError());
     }
+    
+    const accessToken = authorizationHeader.split(' ')[1];
+    if (!accessToken) {
+      console.log('2');
+      return next(ApiError.UnauthorizedError());
+    }
+
+    console.log(accessToken);
+
+    const userData = tokenService.validateAccessToken(accessToken);
+    if (!userData) {
+      console.log('3');
+      return next(ApiError.UnauthorizedError());
+    }
+
+    req.user = userData;
+    next();
+  } catch (e) {
+    return next(ApiError.UnauthorizedError());
+  }
 };
